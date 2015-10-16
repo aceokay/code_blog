af0920c7676d1f263a2cca3831220225ad67e347
Austin <aokincaid@gmail.com>
2015-10-16 10:46:08 -0700

    Build post and tag migration with a join table for future has_and_belongs_to relationship

--- db/migrate/20151016172855_create_post_and_tag.rb
+++ db/migrate/20151016172855_create_post_and_tag.rb
@@ -1,6 +1,21 @@
 class CreatePostAndTag < ActiveRecord::Migration
   def change
-    create_table :post_and_tags do |t|
+    create_table :post do |t|
+      t.column :title, :string
+      t.column :body, :string
+
+      t.timestamps
+    end
+
+    create_table :tag do |t|
+      t.column :category, :string
+
+      t.timestamps
+    end
+
+    create_table :posts_tags, id: false do |t|
+      t.belongs_to :post, index: true
+      t.belongs_to :tag, index: true
     end
   end
 end
