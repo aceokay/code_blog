ac449396f36cdd8ae71dcafb82354391aa828846
Austin <aokincaid@gmail.com>
2015-10-22 22:16:19 -0700

    Add auto-login for new members

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      flash[:notice] = "Welcome to the site!"
-      redirect_to '/'
+    user = User.new(user_params)
+    if user.save
+      @user = User.authenticate(params[:user][:email], params[:user][:password])
+      if @user
+        flash[:notice] = "Welcome to the site!"
+        session[:user_id] = @user.id
+        redirect_to "/"
+      else
+        flash[:alert] = "There was a problem logging you in."
+        redirect_to log_in_path
+      end
     else
       flash[:alert] = "There was a problem creating your account. Please try again."
       redirect_to :back
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,25 @@ class User < ActiveRecord::Base
   validates_confirmation_of :password
   validates :email, uniqueness: true
   before_save :encrypt_password
+  # Enact when user model is updated
+  # before_save :make_admin
 
   def encrypt_password
     self.password_salt = BCrypt::Engine.generate_salt
     self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
   end
+
   def self.authenticate(email, password)
     user = User.where(email: email).first
     if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
       user
     end
   end
+
+  private
+  # def make_admin
+  #   if User.all.length < 1
+  #     self.admin = true
+  #   end
+  # end
 end
