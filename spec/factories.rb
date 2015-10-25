6f576428bc5536ff6e23be188c7d886ca61378ad
Austin <aokincaid@gmail.com>
2015-10-24 19:16:10 -0700

    Add beginning to integration spec for showing tags

--- spec/factories.rb
+++ spec/factories.rb
@@ -8,18 +8,27 @@ FactoryGirl.define do
     admin(true)
   end
 
+  factory :non_admin_user, class: User do
+    name('Duke')
+    sequence :email do |n|
+      (n + rand(100000)).to_s + "@example.com"
+    end
+    password("whose")
+    admin(false)
+  end
+
   factory(:post) do
     title("Love is real")
     body("Build a home at home with love.")
-  end
-
-  factory(:comment) do
-    body("I agree wit chu")
   end
 
   factory(:tag) do
     sequence :category do |n|
       (n + rand(100000) + rand(n)).to_s + "deezl tru"
     end
+  end
+
+  factory(:comment) do
+    body("I agree wit chu")
   end
 end
---
+++ spec/features/show_tag_pages_spec.rb
@@ -0,0 +1,26 @@
+# require 'rails_helper'
+#
+# describe "Show Tag path" do
+#   it "displays a tag and the posts associated with it" do
+#     admin = FactoryGirl.create(:user)
+#     user = FactoryGirl.create(:non_admin_user)
+#     tag = FactoryGirl.create(:tag)
+#     post = FactoryGirl.create(:post)
+#     visit '/'
+#     click_on "Sign In"
+#     fill_in 'Email', :with => admin.email
+#     fill_in 'Password', :with => admin.password
+#     click_on 'Log in'
+#     click_on 'Edit', match: :first
+#     fill_in 'Title', :with => "Two Scoops"
+#     fill_in 'Body', :with => "I love ice-cream and you. :)"
+#     check(tag.category, match: :first)
+#     click_button 'Submit'
+#     click_button 'Log Out'
+#     fill_in 'Email', :with => user.email
+#     fill_in 'Password', :with => user.password
+#     click_on 'Log in'
+#     click_on tag.category
+#     expect(page).to have_content 'Two Scoops'
+#   end
+# end
